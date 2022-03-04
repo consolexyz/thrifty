8e3e19bfb3d3e66288892ba55cabfc4fca1dae26
Moses <mosesobike@gmail.com>
2022-03-04 16:07:51 +0100

    finished sign in amd sign up componets

--- src/components/sign-in-and-sign-up-page/sign-in/SignIn.js
+++ src/components/sign-in-and-sign-up-page/sign-in/SignIn.js
@@ -2,7 +2,7 @@ import React from 'react'
 import FormInput from '../form-input/FormInput'
 import CustomButton from '../../custombutton/CustomButton'
 import './SignIn.scss'
-import {signInWithGoogle} from '../../../firebase/firebase.utils';
+import { auth ,signInWithGoogle} from '../../../firebase/firebase.utils';
 
 class SignIn extends React.Component {
     constructor(props){
@@ -16,7 +16,17 @@ class SignIn extends React.Component {
 
     handleSubmit = event =>{
         event.preventDefault();
-        this.setState({email:'', password: ''})
+        
+        
+    const { email, password } = this.state;
+
+    try {
+     auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.log(error);
+    }
+
     }
     handleChange = event => {
         const {value , name} = event.target;
